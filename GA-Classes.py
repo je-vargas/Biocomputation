29d75d9c2f1815b366ef2daea0aa75ef2264f653
juanE <55924277+je-vargas@users.noreply.github.com>
2021-11-19 12:06:26 +0000

    classes file changed

--- GA-Classes.py
+++ GA-Classes.py
@@ -1,36 +1,14 @@
-import random 
-
-class population: 
+class population:
     def __init__(self):
-        self.__population = []
-        self.__fitness = 0
-
-    @property
-    def get_fitness(self):
-        return self.__fitness
-
-    def set_population(self, population):
-        self.__population = population
-    
-    def population_fitness(self):
-        for individual in self.population:
-            self.fitness += individual.fitness
-        return
+        self.container = list()
+        self.fitness = 0
+        self.size = 0
+        self.averagefitness = 0
+        self.crossOverPoint = list()
 
 class individual_candidate:
     def __init__(self):
-        self.chromosome = []
-        self.__fitness = 0
+        self.genes = list()
+        self.fitness = 0
+        self.relativeFitnessAsPercentage = 0
 
-    def individual_fitness(self):
-        for gene in self.chromosome:
-            if gene:
-                self.fitness += 1
-
-    def chromosome_generator(self, chromosomeLength ):
-        for x in range (0, chromosomeLength ):
-            self.chromosome.append(random.randint(0,1)) # random no between 0-1 because GeneticAlgorithm is using a Binary Encoding
-
-ind = individual_candidate()
-ind.chromosome_generator(10)
-print()
